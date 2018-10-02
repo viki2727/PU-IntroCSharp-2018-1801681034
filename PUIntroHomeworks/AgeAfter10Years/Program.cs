b8b85423bf00a861e60243acc2de457badb75115
fmi <fmi@FMI-535-4>
2018-10-02 16:03:47 +0300

    Included homework t1 & t2.

---
+++ PUIntroHomeworks/AgeAfter10Years/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AgeAfter10Years
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myAge = 19;
+            int after10Years = myAge + 10;
+            Console.WriteLine("My age after 10 years =>" +after10Years);
+        }
+    }
+}
