1037dc68c9bbfebd9b7b58fca32a2c90bd135b2e
viki2727 <43782166+viki2727@users.noreply.github.com>
2018-11-10 14:29:03 +0200

    Commit homework from week 6

---
+++ PUIntroHomeworks/GeometryCalculator/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace GeometryCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string option;
+            Console.WriteLine("Hello user! Choose an option (triangle, square or radians) :");
+            option = Console.ReadLine();
+            switch (option)
+            {
+                case "triangle":
+                    Console.WriteLine("Enter a side:");
+                    double side = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter a height to that side:");
+                    double hside = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"The area of the triangle is: {TriangleArea(side, hside)}");
+                    break;
+
+                case "square":
+                    Console.WriteLine("Enter a side:");
+                    double aSquare = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"The area of the Square is: {SquareArea(aSquare)}");
+                    break;
+
+                case "radians":
+                    Console.WriteLine("Enter an angle's degrees:");
+                    double angle = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"{angle} degrees in radians is: {AngleInRadians(angle)}");
+                    break;
+
+                default:
+                    Console.WriteLine("Sorry, incorrect option! Please choose one of the above");
+                    break;
+            }
+        }
+
+
+        static double TriangleArea(double tSide, double tHeight)
+        {
+            double tArea = tSide * tHeight / 2;
+            return tArea;
+        }
+
+
+        static double SquareArea(double aSqr)
+        {
+            double sSqr = aSqr * aSqr;
+            return sSqr;
+        }
+
+        static double AngleInRadians(double angleDeg)
+        {
+            double angleInRadians = Math.PI / 180 * angleDeg;
+
+            return angleInRadians;
+        }
+    }
+}
---
+++ PUIntroHomeworks/GetMax/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace GetMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 0, number2 = 0, number3 = 0;
+            Console.WriteLine("Hello user! Please enter 3 integers and I will tell you which one is the biggest.");
+            Console.WriteLine(GetTheMax(number1, number2, number3));
+        }
+
+
+
+        public static int GetTheMax(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            Console.WriteLine("Enter first number:");
+            firstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            secondNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter third number:");
+            thirdNumber = int.Parse(Console.ReadLine());
+            if (firstNumber >= secondNumber && firstNumber >= thirdNumber)
+            {
+                return (firstNumber);
+            }
+            else if (secondNumber >= firstNumber && secondNumber >= thirdNumber)
+            {
+                return (secondNumber);
+            }
+            else
+            {
+                return (thirdNumber);
+            }
+        }
+    }
+}
